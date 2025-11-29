3f7ddd479c584ff15cdfd269f184d3526100f8d4
Dylaris <3445818679@qq.com>
2025-11-29 17:19:10 +0800

    draw the background (research how to load float)

--- main.asm
+++ main.asm
@@ -2,9 +2,22 @@ format ELF64
 
 section ".data" writeable
 
-width equ 640
+struc Color r, g, b, a {
+    .r db r
+    .g db g
+    .b db b
+    .a db a
+}
+
+width  equ 640
 height equ 480
-title: db "Hello, World", 0
+title db "Hello, World", 0
+white       Color 255, 255, 255, 255
+green       Color 38,  185, 154, 255
+dark_green  Color 20,  160, 133, 255
+light_green Color 129, 204, 184, 255
+yellow      Color 243, 213, 91,  255
+circle_radius dd 75.0
 
 section ".text" executable
 
@@ -13,6 +26,46 @@ public main
 extrn InitWindow
 extrn CloseWindow
 extrn WindowShouldClose
+extrn BeginDrawing
+extrn EndDrawing
+extrn ClearBackground
+extrn DrawRectangle
+extrn DrawLine
+extrn DrawCircle
+
+draw_background:
+    push rbp
+    mov rbp, rsp
+
+    xor rdi, rdi
+    mov edi, DWORD [dark_green]
+    call ClearBackground
+
+    mov rdi, 0
+    mov rsi, 0
+    mov rdx, width/2
+    mov rcx, height
+    xor r8, r8
+    mov r8d, DWORD [green]
+    call DrawRectangle
+
+    mov rdi, width/2
+    mov rsi, height/2
+    movss xmm0, DWORD [circle_radius]
+    xor rdx, rdx
+    mov edx, [light_green]
+    call DrawCircle
+
+    mov rdi, width/2
+    mov rsi, 0
+    mov rdx, width/2
+    mov rcx, height
+    xor r8, r8
+    mov r8d, DWORD [white]
+    call DrawLine
+
+    pop rbp
+    ret
 
 main:
     push rbp
@@ -23,13 +76,20 @@ main:
     mov rdx, title
     call InitWindow
 
-.begin:
+.event_loop:
     call WindowShouldClose
-    cmp rax, 0
-    jnz .end
-    jmp .begin
-.end:
+    test rax, rax
+    jnz .exit
 
+    call BeginDrawing
+
+    call draw_background
+
+    call EndDrawing
+
+    jmp .event_loop
+
+.exit:
     call CloseWindow
 
     pop rbp
